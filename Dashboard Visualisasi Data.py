4a69223848ade814a7cb1e8a3b25217234bd8a5a
Fadhlydzil <fadhly1204@gmail.com>
2025-07-01 19:22:11 +0700

    Add files via upload

---
+++ Dashboard Visualisasi Data.py
@@ -0,0 +1,20 @@
+import streamlit as st
+import pandas as pd
+import pygwalker as pyg
+import streamlit.components.v1 as components
+
+# Set page layout
+st.set_page_config(page_title="Visualisasi Saham", layout="wide")
+
+# Tambahkan komponen Streamlit
+st.title("📊 Dashboard Visualisasi Volume Saham LQ45")
+st.markdown("Menggunakan Pygwalker Untuk Visualisasi")
+
+# Load data
+df = pd.read_csv("cleaned_saham_data_v2.csv")
+
+# Konversi pygwalker jadi HTML
+html = pyg.to_html(df, spec="Final_Visualisasi_PyGWalker.json")
+
+# Tampilkan dalam Streamlit sebagai komponen HTML
+components.html(html, height=1000, scrolling=True)
